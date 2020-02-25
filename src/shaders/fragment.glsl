246bd684d0d8bb2b0d87aeb81dc86716ee0ef46b
Heavenston <malolegendrelemaire@gmail.com>
2020-02-25 11:20:51 +0100

    removed highp because it doesn't work :(

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,9 +1,9 @@
 precision highp float;
 
-varying highp vec2 v_position;
+varying mediump vec2 v_position;
 
-uniform highp float u_threshold;
-uniform highp float u_ramp;
+uniform float u_threshold;
+uniform float u_ramp;
 
 vec3 startColor = vec3(1., 0.5, 0.);
 vec3 endColor = vec3(0., 0.5, 1.);
@@ -14,14 +14,14 @@ vec3 color(float its) {
 }
 
 void main() {
-  highp vec2 c = v_position;
-  highp vec2 z = vec2(0.);
+  vec2 c = v_position;
+  vec2 z = vec2(0.);
 
   float iterations = 0.;
   for (float i = 0.; i < __ITERATIONS__; i++) {
     iterations = i;
-    highp float zr2 = z.x * z.x;
-    highp float zi2 = z.y * z.y;
+    float zr2 = z.x * z.x;
+    float zi2 = z.y * z.y;
  
     if(zr2 + zi2 > u_threshold) break;
     z = vec2(zr2 - zi2, 2.0 * z.x * z.y) + c;
